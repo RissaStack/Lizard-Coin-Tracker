56576cec2d0634451a4491791399151cf415a03e
Connor <99grogan@gmail.com>
2023-05-03 18:49:57 -0700

    fixed clicking on cryptocurrencies and regenerating elements

--- assets/js/crypto.js
+++ assets/js/crypto.js
@@ -1,3 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var nav = document.getElementById("nav-cc-id");
+    nav.addEventListener("click", function() {
+      console.log("Hello World!");
+    });
+  });
+
 let watchListArr = [];
 
 const url =
@@ -113,17 +120,17 @@ fetch(url)
             row.appendChild(sevenDayChartData);
         }
 
-        
         for (let i = 0; i < data.length; i++) {
             // get star button for each item
             const starButton = document.getElementById(`star-button-${i}`);
-
             // listen for when star is clicked, grabs whole row and checks if it has been clicked already to avoid duplicates
             starButton.addEventListener('click', function (event) {
                 const row = event.target.parentNode.parentNode;
                 console.log(row);
-               
-                const arrIndex = row.getElementsByClassName('coin-number')[0].textContent - 1;
+
+                const arrIndex =
+                    row.getElementsByClassName('coin-number')[0].textContent -
+                    1;
                 if (!watchListArr.includes(arrIndex)) {
                     watchListArr.push(arrIndex);
                     console.log(watchListArr);
@@ -132,14 +139,13 @@ fetch(url)
             });
         }
 
-
         // When Watchlist is clicked current tables are cleared and watchListArr is looped through and appended
-        const navWatchList = document.getElementById('nav-item-id');
+        const navWatchList = document.getElementById('nav-wl-id');
         navWatchList.addEventListener('click', function (event) {
             event.preventDefault();
             const tbody2 = document.querySelector('tbody');
             tbody2.innerHTML = ' ';
-
+            // creates watchlist
             for (let i = 0; i < watchListArr.length; i++) {
                 const currentIndex = watchListArr[i];
                 console.log(currentIndex);
@@ -208,6 +214,126 @@ fetch(url)
                 const coinMktCapSpan = document.createElement('span');
                 coinMktCapSpan.classList.add('coin-mkt-cap');
                 coinMktCapSpan.textContent = data[currentIndex].market_cap;
+
+                const sevenDayChartData = document.createElement('td');
+                sevenDayChartData.classList.add('7-day-chart-td');
+                const sevenDaychartImg = document.createElement('img');
+                sevenDaychartImg.classList.add('sevenDayChart');
+
+                const tbody = document.querySelector('tbody');
+
+                // append
+                tbody.appendChild(row);
+                row.appendChild(starData);
+                row.appendChild(coinNumberData);
+
+                coinInfo.appendChild(coinLogoData);
+                coinInfo.appendChild(coinNameDataSpan);
+                coinInfo.appendChild(coinAbrvData);
+                actualCoinData.appendChild(coinInfo);
+                row.appendChild(actualCoinData);
+
+                coinPriceData.appendChild(coinPriceSpan);
+                actualPriceData.appendChild(coinPriceData);
+                row.appendChild(actualPriceData);
+
+                coin24hrPercentData.appendChild(coin24hrPercentSpan);
+                actual24hrPercentData.appendChild(coin24hrPercentData);
+                row.appendChild(actual24hrPercentData);
+
+                coin7dPercentDataDiv.appendChild(coinPercent7dSpan);
+                actual7dData.appendChild(coin7dPercentDataDiv);
+                row.appendChild(actual7dData);
+
+                coin24hrVolumeDataDiv.appendChild(coin24hrVolumeSpan);
+                actual24hrVolumeData.appendChild(coin24hrVolumeDataDiv);
+                row.appendChild(actual24hrVolumeData);
+
+                coinMktCapDataDiv.appendChild(coinMktCapSpan);
+                actualMktCapData.appendChild(coinMktCapDataDiv);
+                row.appendChild(actualMktCapData);
+
+                sevenDayChartData.appendChild(sevenDaychartImg);
+                row.appendChild(sevenDayChartData);
+            }
+        });
+
+        //regenerates cc page
+        const navCc = document.getElementById('nav-cc-id');
+        navCc.addEventListener('click', function (event) {
+            event.preventDefault();
+            const tbody2 = document.querySelector('tbody');
+            tbody2.innerHTML = ' ';
+
+            for (let i = 0; i < data.length; i++) {
+                // const currentIndex = watchListArr[i];
+                // console.log(currentIndex);
+                const row = document.createElement('tr');
+                console.log(row);
+
+                const starData = document.createElement('td');
+                starData.classList.add('star');
+                starData.innerHTML = `<button class="unfilled" id="star-button-${i}"></button>`;
+                const coinNumberData = document.createElement('td');
+                coinNumberData.classList.add('coin-number');
+                coinNumberData.textContent = i + 1;
+
+                const actualCoinData = document.createElement('td');
+                actualCoinData.classList.add('actual-coin');
+                const coinInfo = document.createElement('div');
+                coinInfo.classList.add('coin-info-div');
+                const coinLogoData = document.createElement('img');
+                coinLogoData.classList.add('coin-logo');
+                const coinNameDataSpan = document.createElement('span');
+                coinNameDataSpan.classList.add('coin-name');
+                coinNameDataSpan.textContent = data[i].id;
+                const coinAbrvData = document.createElement('span');
+                coinAbrvData.classList.add('coin-abrv');
+                coinAbrvData.textContent = data[i].symbol;
+
+                const actualPriceData = document.createElement('td');
+                actualPriceData.classList.add('actual-price-td');
+                const coinPriceData = document.createElement('div');
+                coinPriceData.classList.add('coin-price-data-div');
+                const coinPriceSpan = document.createElement('span');
+                coinPriceSpan.classList.add('coin-price');
+                coinPriceSpan.textContent = data[i].current_price;
+
+                const actual24hrPercentData = document.createElement('td');
+                actual24hrPercentData.classList.add('actual-24hr-%');
+                const coin24hrPercentData = document.createElement('div');
+                coin24hrPercentData.classList.add('coin-24hr-percent-data-div');
+                const coin24hrPercentSpan = document.createElement('span');
+                coin24hrPercentSpan.classList.add('coin-percent-24hr');
+                coin24hrPercentSpan.textContent =
+                    data[i].price_change_percentage_24h;
+
+                const actual7dData = document.createElement('td');
+                actual7dData.classList.add('acutal-7d-%');
+                const coin7dPercentDataDiv = document.createElement('div');
+                coin7dPercentDataDiv.classList.add('coin-7d-percent-data-div');
+                const coinPercent7dSpan = document.createElement('span');
+                coinPercent7dSpan.classList.add('coin-percent-7d');
+                coinPercent7dSpan.textContent = data[i].ath;
+
+                const actual24hrVolumeData = document.createElement('td');
+                actual24hrVolumeData.classList.add('actual-24hr-volume');
+                const coin24hrVolumeDataDiv = document.createElement('div');
+                coin24hrVolumeDataDiv.classList.add(
+                    'coin-24hr-volume-data-div'
+                );
+                const coin24hrVolumeSpan = document.createElement('span');
+                coin24hrVolumeSpan.classList.add('coin-24hr-volume');
+                coin24hrVolumeSpan.textContent =
+                    data[i].total_volume;
+
+                const actualMktCapData = document.createElement('td');
+                actualMktCapData.classList.add('actual-mkt-cap');
+                const coinMktCapDataDiv = document.createElement('div');
+                coinMktCapDataDiv.classList.add('coin-mkt-cap-data-div');
+                const coinMktCapSpan = document.createElement('span');
+                coinMktCapSpan.classList.add('coin-mkt-cap');
+                coinMktCapSpan.textContent = data[i].market_cap;
 
                 const sevenDayChartData = document.createElement('td');
                 sevenDayChartData.classList.add('7-day-chart-td');
